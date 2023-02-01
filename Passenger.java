9b3d8f361ae8464851ca6377b56de93dcdb9afa1
Juliet Lord <jlord24@students.polytechnic.org>
2023-02-01 09:25:52 -0800

    getFinal() method

--- Passenger.java
+++ Passenger.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class passenger{
     //fields
-    private double finalDestination;
-    private double start;
+    private int finalDestination;
+    private int start;
 
     //constructors
     public passenger(){
-        finalDestination = Math.random() * 31 + 1;
-        start  = Math.random() * 31 + 1;
+        finalDestination = (int)Math.random() * 31 + 1;
+        start  = (int)Math.random() * 31 + 1;
     }
 
     //methods
+
+    public int getFinal(){
+        return finalDestination;
+    }
 }
