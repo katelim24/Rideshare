0119d859c81592ac729c515511564b05b1f3af47
Kate Lim <klim24@students.polytechnic.org>
2023-02-01 09:22:48 -0800

    added constructor

--- Passenger.java
+++ Passenger.java
@@ -1,9 +1,15 @@
+import java.util.*; 
+
 public class passenger{
     //fields
-    private int finalDestination;
-    private int start;
+    private double finalDestination;
+    private double start;
 
     //constructors
+    public passenger(){
+        finalDestination = Math.random() * 31 + 1;
+        start  = Math.random() * 31 + 1;
+    }
 
     //methods
 }
