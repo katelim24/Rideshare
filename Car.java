cb1b23074d80d048146425a17e56791c9724bc90
Kate Lim <klim24@students.polytechnic.org>
2023-02-02 12:31:14 -0800

    edited passenger and car

---
+++ Car.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Car {
+    //fields
+    private int numPassengers;
+    private ArrayList<Passenger> passList;
+    private int moneyEarned;
+    private int location;
+    private int start;
+    private int finalDestination;
+    private boolean isGoingForward;
+
+    //constructors
+    public Car(){
+        Car c = new Car();
+        numPassengers = 0;
+        moneyEarned = 0;
+        start = (int)Math.random() * 31 + 1;
+        location = start;
+        finalDestination = (int)Math.random() * 31 + 1;
+        isGoingForward = true;
+    }
+
+    //methods
+    public void drive(){
+        location += 1;
+    }
+
+    public void pickup(Station a, Passenger b){
+        a.removePassenger(location);
+        passList.add(b);
+    }
+
+}
--- Passenger.java
+++ Passenger.java
@@ -3,13 +3,11 @@ public class Passenger {
     //fields
     private int finalDestination;
     private int start;
-    private int current;
 
     //constructors
     public Passenger(){
         finalDestination = (int)Math.random() * 31 + 1;
         start  = (int)Math.random() * 31 + 1;
-        current = 0;
     }
 
     //methods
