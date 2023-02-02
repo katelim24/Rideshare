d300eccbecf2aa1b022109e0f682b733ff114e72
Juliet Lord <jlord24@students.polytechnic.org>
2023-02-02 12:19:23 -0800

    testing station in runner file

--- Runner.java
+++ Runner.java
@@ -1,5 +1,16 @@
 public class Runner {
     public static void main(String[] args) {
+        Passenger one = new Passenger();
+        Passenger two = new Passenger();
+        Passenger three = new Passenger();
+
+        
         Station a = new Station();
+        a.addPassenger(one);
+        a.addPassenger(two);
+        a.addPassenger(three);
+        System.out.println("Added 3 passengers..." + a.toString());
+        a.removePassenger(passengers[1]);
+
     }
 }
