ac97a83997585dfbeedbad265154639edcf17fb3
Kate Lim <klim24@students.polytechnic.org>
2023-02-02 11:49:21 -0800

    edited passenger

--- Passenger.java
+++ Passenger.java
@@ -3,16 +3,22 @@ public class Passenger {
     //fields
     private int finalDestination;
     private int start;
+    private int current;
 
     //constructors
     public Passenger(){
         finalDestination = (int)Math.random() * 31 + 1;
         start  = (int)Math.random() * 31 + 1;
+        current = 0;
     }
 
     //methods
 
     public int getFinal(){
         return finalDestination;
+    }
+
+    public String toString(){
+        return "Starting station: " + start + ", final destination: " + finalDestination + ", current position: " + current;
     }
 }
