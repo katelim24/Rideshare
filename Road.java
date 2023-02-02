e668a76c90ebef9ac4be804f1d63e887f62a41e9
Kate Lim <klim24@students.polytechnic.org>
2023-02-02 11:59:16 -0800

    edited road

---
+++ Road.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class road {
+    //fields
+    private Station[] stops;
+    private int length;
+    private car[] cars;
+
+    //constructors
+    public road(){
+        Station[] stops = new Station[31];
+    }
+
+    //methods
+    public void update(){
+
+    }
+}
