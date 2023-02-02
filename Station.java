ae96b9b63c6813afc701eed4417b342210dc2fd9
Juliet Lord <jlord24@students.polytechnic.org>
2023-02-02 11:58:51 -0800

    wrote station file

--- Station.java
+++ Station.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class Station {
-    
+    //fields
+    private ArrayList<Passenger> passengers;
+
+    //constructors
+    public Station(){
+        passengers = new ArrayList<Passenger>();
+    }
+
+
+    //methods
+    public void addPassenger(Passenger a){
+        passengers.add(a);
+    }
+
+    public void removePassenger(int pos){
+        passengers.remove(pos);
+    }
+
+    public ArrayList<Passenger> getList(){
+        return passengers;
+    }
 }
