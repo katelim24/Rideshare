04c5c219427de3c0bab587aec1a8cd105742fdf3
Kate Lim <klim24@students.polytechnic.org>
2023-02-06 13:25:08 -0800

    started adding comments to methods

--- Station.java
+++ Station.java
@@ -13,19 +13,39 @@ public class Station {
 
 
     //methods
+    /**
+     * this adds a new passenger to the passenger list
+     * @param a the passanger to be added in
+     */
     public void addPassenger(Passenger a){
         passengers.add(a);
     }
 
+    /**
+     * this removes a passenger from the passenger list
+     * @param pos the position of the passenger to be removed
+     */
     public void removePassenger(int pos){
         passengers.remove(pos);
     }
 
+    /**
+     * 
+     * @return
+     */
     public ArrayList<Passenger> getList(){
         return passengers;
     }
 
     public String toString(){
-        return "Passengers" + passengers + "Station num" + num;
+        return "Passengers: " + passengers + "Station num: " + num;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Passenger nextLeftPassenger(){
+
     }
 }
