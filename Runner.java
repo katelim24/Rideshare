83621404c2013032342f0607c70085cb97673895
Juliet Lord <jlord24@students.polytechnic.org>
2023-02-06 13:54:54 -0800

    tested car

--- Runner.java
+++ Runner.java
@@ -1,11 +1,13 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Testing Passenger...");
-        Passenger one = new Passenger(0,7);
-        Passenger two = new Passenger(7,5);
-        Passenger three = new Passenger(2,9);
+        Passenger one = new Passenger();
+        Passenger two = new Passenger();
+        Passenger three = new Passenger();
 
         
+
+
         System.out.println("Testing station...");
         Station a = new Station(5);
         a.addPassenger(one);
@@ -13,6 +15,20 @@ public class Runner {
         a.addPassenger(three);
         System.out.println("Added 3 passengers..." + a.toString());
         a.removePassenger(1);
+
+
+        System.out.println("Testing the road...");
+        Road r = new Road();
+
+        System.out.println("Testing the car...");
+        Car c = new Car();
+        System.out.println("Printing the current road...");
+        System.out.println(r.toString());
+        c.drive();
+        c.pickup(a, one);
+        //c.dropoff;
+
+
 
     }
 }
