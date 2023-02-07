364cb1b3e2fb13d82cb2a03ef2e34499af7957be
Kate Lim <klim24@students.polytechnic.org>
2023-02-07 14:59:01 -0800

    added tests to runner

--- Runner.java
+++ Runner.java
@@ -2,5 +2,15 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Testing the road...");
         Road r = new Road();
+
+        System.out.println("Printing the starting road -->");
+        System.out.println();
+        r.populate();
+        System.out.println(r);
+
+        System.out.println("Moving -->");
+        System.out.println();
+        r.update();
+        System.out.println(r);
     }
 }
